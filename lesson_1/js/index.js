ad046dee1ae234ce10eaa93e60361e283254ec34
theinternethero <41805904+theinternethero@users.noreply.github.com>
2019-05-22 13:00:46 +0400

    add self

--- lesson_1/js/index.js
+++ lesson_1/js/index.js
@@ -1,36 +1,31 @@
 'use strict';
 
-let money = +prompt('Ваш бюджет на месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
+let money = +prompt('Ваш бюджет на месяц?'),
+    time = +prompt('Введите дату в формате YYYY-MM-DD');
 
 let appData = {
-    budget: money,
+    moneyData: money, // для того чтобы добавить переменную нужно сначала создать св-во
     timeData: time,
-    expenses: {},
+    expenses: {}, // можно объявлять пустые объекты внутри объекта, а потом добавлять им свойства
     optionalExpenses: {},
-    income: [],
+    income: [], // можно добавлять пустые массивы
     savings: false
 };
 
-let costs = +prompt('Введите обязательную статью расходов в этом месяце');
-let dolg = +prompt('Во сколько обойдётся?');
-let costsTwo = +prompt('Введите обязательную статью расходов в этом месяце');
-let dolgTwo = +prompt('Во сколько обойдётся?');
+let necessarilyMoneyFirst = +prompt('Введите обязательную статью расходов в этом месяце'),
+    howMuchFirst = +prompt('Во сколько обойдется?'),
+    necessarilyMoneySecond = +prompt('Введите обязательную статью расходов в этом месяце'),
+    howMuchSecond = +prompt('Во сколько обойдется?');
 
-appData.expenses.costs = dolg;
-appData.expenses.costsTwo = dolgTwo;
+// Мой вариант (неправильно):
+// expenses = {
+//     necessarilyMoneyFirst: howMuchFirst,
+//     necessarilyMoneySecond: howMuchSecond
+// };
 
-alert(appData.budget/30);
+appData.expenses.necessarilyMoneyFirst = howMuchFirst; // = знак присваивания
+appData.expenses.necessarilyMoneySecond = howMuchSecond;
 
+console.log(appData.expenses.necessarilyMoneyFirst);
 
-
-
-
-
-
-
-
-
-
-
-
+alert(appData.moneyData / 30);
